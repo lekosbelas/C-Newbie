20c3579118ab8877984be819e46705d9054725a1
Muhammet Hasan Taşkıran <35641898+lekosbelas@users.noreply.github.com>
2023-01-24 03:28:35 +0300

    Update main.c

--- Area of Rectangle/main.c
+++ Area of Rectangle/main.c
@@ -5,13 +5,10 @@ int main(){
     int lenght;
     int breadth;
     
-     // get length and breadth input
     scanf("%d %d", &lenght, &breadth);
     
-    // find the area
     int Area = lenght * breadth;
     
-    // print the area
     printf("%d", Area);
 
     return 0;    
